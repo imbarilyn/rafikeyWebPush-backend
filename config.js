a384c77c8228ca8e9b950e6923e2704898963183
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-25 07:49:37 +0300

    add config file with env variables

---
+++ config.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import  path from 'path';
+
+dotenv.config({path: path.resolve(process.cwd(), '.env')})
+
+
+export const config = {
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DATABASE: process.env.DATABASE,
+    DB_PORT: process.env.DB_PORT,
+    SERVER_PORT: process.env.SERVER_PORT,
+    VAPID_PUBLIC_KEY: process.env.VAPID_PUBLIC_KEY,
+    VAPID_PRIVATE_KEY: process.env.VAPID_PRIVATE_KEY,
+    BACKEND_BASE_URL: process.env.BACKEND_BASE_URL
+}
+
