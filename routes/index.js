0c98aa63a193e8dd54260ff136dc372d5206b3f9
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-27 16:42:20 +0300

    remove write to file for success result

--- routes/index.js
+++ routes/index.js
@@ -22,43 +22,44 @@ webPush.setVapidDetails(
     vapidKeys.privateKey
 )
 const filepath =
-router.get('/send-notifications', async (req, res) => {
-    try {
-        // console.log('This is directory:', fs.__dirname)
-        const [rows] = await db.execute("SELECT * FROM subscriptions");
-        rows.forEach((row) => {
-            // sendNotification(row)
-            const subscriptionBody = {
-                endpoint: row.endpoint,
-                keys: {
-                    p256dh: row.p256dh,
-                    auth: row.auth
-                }
-            }
-            const dataToSend = JSON.stringify({
-                    title: 'Chat Clean up Notice',
-                    body: 'Please be notified that chats at least 30days old will be deleted in less than 24 hours'
-                }
-            )
-            webPush.sendNotification(subscriptionBody, dataToSend)
-                .then((response) => {
-                    fs.writeFileSync(path.resolve(__dirname, '../log.txt'), `\n notification send successfully to: ${row.user} at ${new Date().toISOString()}`, 'utf-8', (err)=>{
-                        console.log('Error writing to log file', err)
-                    })
-                })
-                // delete any stale subscription from the database
-                .catch((err) => {
-                    if(err.statusCode === 401 || err.statusCode === 410) {
-                        db.query('DELETE FROM subscriptions WHERE endpoint = ?', [row.endpoint])
+    router.get('/send-notifications', async (req, res) => {
+        try {
+            // console.log('This is directory:', fs.__dirname)
+            const result = await pool.query("SELECT * FROM subscriptions");
+            const rows = result.rows
+            await Promise.all(rows.map(async row => {
+                // sendNotification(row)
+                const subscriptionBody = {
+                    endpoint: row.endpoint,
+                    keys: {
+                        p256dh: row.p256dh,
+                        auth: row.auth
                     }
-                })
-        })
-    } catch (err) {
-        fs.writeFileSync(path.resolve(__dirname, '../log.txt'), `\n Error in sending notifications at ${new Date().toISOString()}: ${err.message}`, 'utf-8', (err)=>{
-            console.log('Error writing to log file', err)
-        })
-    }
-})
+                }
+                const dataToSend = JSON.stringify({
+                        title: 'Chat Clean up Notice',
+                        body: 'Please be notified that chats at least 30days old will be deleted in less than 24 hours'
+                    }
+                )
+                try {
+                    await webPush.sendNotification(subscriptionBody, dataToSend)
+
+                } catch (err) {
+                    if (err.statusCode === 401 || err.statusCode === 410) {
+                        pool.query('DELETE FROM subscriptions WHERE endpoint = $1', [row.endpoint])
+                        // fs.appendFileSync(path.resolve(__dirname, `../log.txt`), `\n Subscription removed for endpoint: ${row.endpoint} at ${new Date().toISOString()}`, 'utf8');
+                    }
+
+                }
+
+
+            }))
+            // res.status(200).json({message: 'Notifications sent successfully'})
+        } catch (err) {
+            fs.appendFileSync(path.resolve(__dirname, `../log.txt`), `\n Error in sending notifications at ${new Date().toISOString()}: ${err.message}`, 'utf8');
+            res.status(500).json({ message: 'Failed to send notifications' });
+        }
+    })
 
 // unsubscribe the user from push notifications
 router.delete('/unsubscribe', async (req, res) => {
