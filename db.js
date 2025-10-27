af3cb92ef7993f5a942c5f09e501102d4a1e63e9
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-27 18:29:37 +0300

    add render database url

--- db.js
+++ db.js
@@ -6,14 +6,23 @@ import pkg from 'pg'
 const { Pool } = pkg
 
 
-export const  pool = await  new Pool({
-    host: config.DB_HOST,
-    user: config.DB_USER,
-    password: config.DB_PASSWORD,
-    database: config.DATABASE,
-    port: config.DB_PORT,
-    max:10,
-    min: 4,
-    idleTimeoutMillis: 600000,
+// export const  pool = await  new Pool({
+//     host: config.DB_HOST,
+//     user: config.DB_USER,
+//     password: config.DB_PASSWORD,
+//     database: config.DATABASE,
+//     port: config.DB_PORT,
+//     max:10,
+//     min: 4,
+//     idleTimeoutMillis: 600000,
+//
+// })
 
-})
+export const pool = ()=>{
+    return new Pool ({
+        connectionString: config.DATABASE_URL_RENDER,
+        ssl: {
+            rejectUnauthorized: false
+        },
+    })
+}
