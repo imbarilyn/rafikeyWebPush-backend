d0e3437ee0fb7c6c71926cec93ec294df72dc990
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-28 14:27:39 +0300

    update database connection

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
 import { config } from "./config.js"
 // import mysql from "mysql2/promise";
-import pkg from 'pg'
+import pg from 'pg'
 
 
-const { Pool } = pkg
+const { Pool } = pg
 
+
+export const  pool = await  new Pool({
+    host: config.DB_HOST,
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    database: config.DATABASE,
+    port: config.DB_PORT,
+    max:10,
+    min: 4,
+    idleTimeoutMillis: 600000,
 
 // export const  pool = await  new Pool({
 //     host: config.DB_HOST,
