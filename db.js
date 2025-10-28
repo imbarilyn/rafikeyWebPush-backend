b6bc57cf929fb305a6e34e328134302e6ad7af00
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-28 14:27:57 +0300

    update database connection

--- db.js
+++ db.js
@@ -16,23 +16,13 @@ export const  pool = await  new Pool({
     min: 4,
     idleTimeoutMillis: 600000,
 
-// export const  pool = await  new Pool({
-//     host: config.DB_HOST,
-//     user: config.DB_USER,
-//     password: config.DB_PASSWORD,
-//     database: config.DATABASE,
-//     port: config.DB_PORT,
-//     max:10,
-//     min: 4,
-//     idleTimeoutMillis: 600000,
-//
-// })
+})
 
-export const pool = ()=>{
-    return new Pool ({
-        connectionString: config.DATABASE_URL_RENDER,
-        ssl: {
-            rejectUnauthorized: false
-        },
-    })
-}
+// export const pool = ()=>{
+//     return new Pool ({
+//         connectionString: config.DATABASE_URL_RENDER,
+//         ssl: {
+//             rejectUnauthorized: false
+//         },
+//     })
+// }
